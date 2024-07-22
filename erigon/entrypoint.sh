d09a962a755e310bbdf2d313ef8b36a66522616f
dappnodedev <diego@dappnode.io>
2024-07-22 17:51:03 +0200

    Use latest staker scripts version

--- erigon/entrypoint.sh
+++ erigon/entrypoint.sh
@@ -5,27 +5,26 @@ SUPPORTED_NETWORKS="holesky mainnet gnosis"
 # shellcheck disable=SC1091
 . /etc/profile
 
-run_client() {
-    exec erigon --datadir="${DATADIR}" \
-        --http.addr=0.0.0.0 \
-        --http.vhosts=* \
-        --http.corsdomain=* \
-        --ws \
-        --private.api.addr=0.0.0.0:9090 \
-        --metrics \
-        --metrics.addr=0.0.0.0 \
-        --metrics.port=6060 \
-        --pprof \
-        --pprof.addr=0.0.0.0 \
-        --pprof.port=6061 \
-        --port="${P2P_PORT}" \
-        --authrpc.jwtsecret="${JWT_PATH}" \
-        --authrpc.addr 0.0.0.0 \
-        --authrpc.vhosts=* \
-        --db.size.limit=8TB \
-        "${EXTRA_OPTS}"
-}
+JWT_PATH=$(get_jwt_path "${NETWORK}" "${SUPPORTED_NETWORKS}" "${SECURITY_PATH}")
 
-set_execution_config_by_network "${SUPPORTED_NETWORKS}"
-post_jwt_to_dappmanager
-run_client
+post_jwt_to_dappmanager "${JWT_PATH}"
+
+# shellcheck disable=SC2086
+exec erigon --datadir="${DATA_DIR}" \
+    --chain="${NETWORK}" \
+    --http.addr=0.0.0.0 \
+    --http.vhosts=* \
+    --http.corsdomain=* \
+    --ws \
+    --private.api.addr=0.0.0.0:9090 \
+    --metrics \
+    --metrics.addr=0.0.0.0 \
+    --metrics.port=6060 \
+    --pprof \
+    --pprof.addr=0.0.0.0 \
+    --pprof.port=6061 \
+    --port="${P2P_PORT}" \
+    --authrpc.jwtsecret="${JWT_PATH}" \
+    --authrpc.addr 0.0.0.0 \
+    --authrpc.vhosts=* \
+    --db.size.limit=8TB ${EXTRA_OPTS}
